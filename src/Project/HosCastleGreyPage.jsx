59c643af424a482bf325237a1f3886e5bc4d3d27
Awasthi Group <socialthekaaccess.com>
2026-09-23 17:47:27 +0530

    Castle Grey: replace old images with CASTLEGREY1-9 new images

--- src/Project/HosCastleGreyPage.jsx
+++ src/Project/HosCastleGreyPage.jsx
@@ -1,13 +1,13 @@
 import ProjectDetailPage from '../pages/ProjectDetailPage'
-import img1 from '../../assets/projects/hos-cg-1.avif'
-import img2 from '../../assets/projects/hos-cg-2.avif'
-import img3 from '../../assets/projects/hos-cg-3.avif'
-import img4 from '../../assets/projects/hos-cg-4.avif'
-import img5 from '../../assets/projects/hos-cg-5.avif'
-import img6 from '../../assets/projects/hos-cg-6.avif'
-import img7 from '../../assets/projects/hos-cg-7.avif'
-import img8 from '../../assets/projects/hos-cg-8.avif'
-import img9 from '../../assets/projects/hos-cg-9.avif'
+import img1 from '../../assets/projects/CASTLEGREY1.png'
+import img2 from '../../assets/projects/CASTLEGREY2.png'
+import img3 from '../../assets/projects/CASTLEGREY3.png'
+import img4 from '../../assets/projects/CASTLEGREY4.png'
+import img5 from '../../assets/projects/CASTLEGREY5.png'
+import img6 from '../../assets/projects/CASTLEGREY6.png'
+import img7 from '../../assets/projects/CASTLEGREY7.png'
+import img8 from '../../assets/projects/CASTLEGREY8.png'
+import img9 from '../../assets/projects/CASTLEGREY9.png'
 
 export default function HosCastleGreyPage() {
   return (
